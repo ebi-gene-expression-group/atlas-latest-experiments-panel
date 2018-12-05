2e1b4124c9074d42a154248b4b73da358e709111
lingyun1010 <zhaolingyun1010@gmail.com>
2018-12-05 10:23:19 +0000

    Reformat html tag style in jsx

--- src/cards/LatestExperimentCard.js
+++ src/cards/LatestExperimentCard.js
@@ -4,16 +4,16 @@ import latestExperimentCardPropTypes from './latestExperimentCardPropTypes'
 
 const LatestExperimentCard = ({numberOfAssays, host, experimentAccession, lastUpdate, experimentDescription, species}) => 
   <li>
-    <div className="media-object stack-for-small">
-      <div className="media-object-section middle hide-for-small-only">
-        <span className="button secondary no-action fixed-width-medium has-tip" data-tooltip="fmjj2g-tooltip" title="" 
-          aria-describedby="o0u550-tooltip" data-yeti-box="o0u550-tooltip" data-toggle="o0u550-tooltip" 
-          data-resize="o0u550-tooltip" data-events="resize">{numberOfAssays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} assays</span>
+    <div className={`media-object stack-for-small`}>
+      <div className={`media-object-section middle hide-for-small-only`}>
+        <span className={`button secondary no-action fixed-width-medium has-tip`} data-tooltip={`fmjj2g-tooltip`} title={``} 
+          aria-describedby={`o0u550-tooltip`} data-yeti-box={`o0u550-tooltip`} data-toggle={`o0u550-tooltip`} 
+          data-resize={`o0u550-tooltip`} data-events={`resize`}>{numberOfAssays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} assays</span>
       </div>
-      <div className="media-object-section middle hide-for-small-only ">
-        <a className="button" href={`${host}/experiments/${experimentAccession}`}>Results</a>
+      <div className={`media-object-section middle hide-for-small-only`}>
+        <a className={`button`} href={`${host}/experiments/${experimentAccession}`}>Results</a>
       </div>
-      <div className="media-object-section middle">
+      <div className={`media-object-section middle`}>
         <small>{lastUpdate}</small><br/>
         <a href={`${host}/experiments/${experimentAccession}`}>
           {experimentDescription} <i>{species}</i>
