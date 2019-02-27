ed1d67bcad5644c0b0c3e155581afe96b7910c36
lingyun1010 <zhaolingyun1010@gmail.com>
2019-02-27 14:25:05 +0000

    Refactor code with styled-components and backtick

--- src/SceaHomePageLatestExperimentPanel.js
+++ src/SceaHomePageLatestExperimentPanel.js
@@ -1,30 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const TableCellDiv = styled.div`
+{
+ padding-right: 0px;
+ vertical-align: middle;
+ display: table-cell;
+}
+`
 
 const LatestExperimentPanel = ({numberOfAssays, host, experimentAccession, lastUpdate, experimentDescription, species}) =>
     <li>
-        <div style={{display: 'block', marginBottom: '1rem'}}>
-            <div className={'hide-for-small-only'} style={{paddingRight: '0px', verticalAlign: 'middle', display: 'table-cell'}}>
-                <span className={'button'} style={{marginBottom: '0px', width: '120px', cursor: 'default', backgroundColor: '#777'}}
-                    data-tooltip title={'Number of assays in experiment'}>
-
-                    {numberOfAssays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} assays</span>
-            </div>
-            <div className={'hide-for-small-only'} style={{paddingRight: '0px', verticalAlign: 'middle', display: 'table-cell'}}>
-                <a className={'button'} style={{marginBottom: '0px', backgroundColor: `#3497c5`}} href={`${host}/experiments/${experimentAccession}`}>Results</a>
-            </div>
-            <div style={{paddingRight: '0px', paddingLeft: '1rem', verticalAlign: 'middle', display: 'table-cell'}}>
+        <div style={{display: `block`, marginBottom: `1rem`}}>
+            <TableCellDiv className={`hide-for-small-only`}>
+                <span className={`button`} style={{marginBottom: `0px`, width: `120px`, cursor: `default`, backgroundColor: `#777`}}
+                    data-tooltip title={`Number of assays in experiment`}>
+                    {numberOfAssays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, `,`)} assays</span>
+            </TableCellDiv>
+            <TableCellDiv className={`hide-for-small-only`}>
+                <a className={`button`} style={{marginBottom: `0px`, backgroundColor: `#3497c5`}} href={`${host}/experiments/${experimentAccession}`}>Results</a>
+            </TableCellDiv>
+            <TableCellDiv style={{paddingLeft: `1rem`}}>
                 <small>{lastUpdate}</small><br/>
                 <a href={`${host}/experiments/${experimentAccession}`}>
                     {experimentDescription} <i>{species}</i>
                 </a>
-            </div>
+            </TableCellDiv>
         </div>
     </li>
 
 const SceaHomePageLatestExperimentPanel = ({cards, host}) =>
-    <div id={'latest-experiments'} style={{padding: '1rem'}}>
-        <ul style={{listStyle: 'none', marginLeft: 'offset'}}>
+    <div id={`latest-experiments`} style={{padding: `1rem`}}>
+        <ul style={{listStyle: `none`, marginLeft: `offset`}}>
             { Array.isArray(cards) && cards.map((card, idx) => <LatestExperimentPanel key={idx} host={host} {...card}/>) }
         </ul>
     </div>
