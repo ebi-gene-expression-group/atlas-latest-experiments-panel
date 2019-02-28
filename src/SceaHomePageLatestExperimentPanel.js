3ea6c69ea2b5931633ba4f6bd01acd966c0c6798
lingyun1010 <zhaolingyun1010@gmail.com>
2019-02-28 14:28:20 +0000

    Delete eslintrc file and clean up code style

--- src/SceaHomePageLatestExperimentPanel.js
+++ src/SceaHomePageLatestExperimentPanel.js
@@ -11,12 +11,11 @@ const TableCellDiv = styled.div`
 `
 
 const LatestExperimentEntry = ({numberOfAssays, host, experimentAccession, lastUpdate, experimentDescription, species}) =>
- <li>
-   <div style={{display: `block`, marginBottom: `1rem`}}>
+  <li>
+    <div style={{display: `block`, marginBottom: `1rem`}}>
       <TableCellDiv className={`hide-for-small-only`}>
-        <span className={`button`} style={{marginBottom: `0px`, width: `120px`, cursor: `default`, backgroundColor: `#777`}}
-          data-tooltip title={`Number of assays in experiment`}>
-            numberOfAssays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, `,`)} assays</span>
+        <span className={`button`} style={{marginBottom: `0px`, width: `120px`, cursor: `default`, backgroundColor: `#777`}} data-tooltip title={`Number of assays in experiment`}>
+          {numberOfAssays.toString().replace(/\B(?=(\d{3})+(?!\d))/g, `,`)} assays</span>
       </TableCellDiv>
       <TableCellDiv className={`hide-for-small-only`}>
         <a className={`button`} style={{marginBottom: `0px`, backgroundColor: `#3497c5`}} href={`${host}/experiments/${experimentAccession}`}>Results</a>
@@ -27,13 +26,12 @@ const LatestExperimentEntry = ({numberOfAssays, host, experimentAccession, lastU
           {experimentDescription} <i>{species}</i>
         </a>
       </TableCellDiv>
-   </div>
- </li>
+    </div>
+  </li>
 
 const SceaHomePageLatestExperimentPanel = ({cards, host}) => <div id={`latest-experiments`} style={{padding: `1rem`}}>
   <ul style={{listStyle: `none`, marginLeft: `offset`}}>
-    { Array.isArray(cards) && cards.map((card, idx) => <LatestExperimentEntry key={idx} host={host} {...card}/>) }
-  </ul>
+    { Array.isArray(cards) && cards.map((card, idx) => <LatestExperimentEntry key={idx} host={host} {...card}/>) }</ul>
 </div>
 
 
@@ -52,8 +50,8 @@ SceaHomePageLatestExperimentPanel.propTypes = {
 }
 
 SceaHomePageLatestExperimentPanel.propTypes = {
-    cards: PropTypes.arrayOf(PropTypes.shape(LatestExperimentEntry.propTypes)).isRequired,
-    host: PropTypes.string.isRequired
+  cards: PropTypes.arrayOf(PropTypes.shape(LatestExperimentEntry.propTypes)).isRequired,
+  host: PropTypes.string.isRequired
 }
 
 export default SceaHomePageLatestExperimentPanel
